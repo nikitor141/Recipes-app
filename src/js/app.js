db4215e7837344e42accea12618527ed0ec6ad5f
nikitor141 <nikitor141@gmail.com>
2025-10-04 18:59:59 +0300

    improved ui. Added ultimate pancakes recipe

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,19 @@
-import Swiper from 'swiper'
-import { Pagination } from 'swiper/modules'
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
 
-import * as webpTest from './modules/iswebp.js'
+import * as webpTest from "./modules/iswebp.js";
 
-webpTest.isWebp()
+webpTest.isWebp();
 
 //theme switcher
-let dataTheme = document.documentElement.dataset
-const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
+let dataTheme = document.documentElement.dataset;
+const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
 
 function setTheme(dataTheme) {
-	if (dataTheme.theme === 'light' || darkThemeMq.matches) {
-		dataTheme.theme = 'dark'
+	if (dataTheme.theme === "light" || darkThemeMq.matches) {
+		dataTheme.theme = "dark";
 	} else {
-		dataTheme.theme = 'light'
+		dataTheme.theme = "light";
 	}
 }
 // setTheme(dataTheme)
@@ -24,18 +24,32 @@ const sliderOptions = {
 	autoHeight: true,
 	modules: [Pagination],
 	pagination: {
-		el: '.swiper-pagination',
-		type: 'bullets',
-		clickable: true
-	}
-}
-new Swiper('.swiper', sliderOptions)
+		el: ".swiper-pagination",
+		type: "bullets",
+		clickable: true,
+	},
+};
+new Swiper(".swiper", sliderOptions);
 
-let rem
+let rem;
 function setRem() {
-	rem = +parseFloat(getComputedStyle(document.documentElement).fontSize).toFixed(2)
+	rem = +parseFloat(
+		getComputedStyle(document.documentElement).fontSize,
+	).toFixed(2);
 }
 
-const stickyElement = document.querySelector('.recipe__ingredients-column')
+const stickyElement = document.querySelector(".recipe__ingredients-column");
 
-setRem()
+setRem();
+
+const center = { x: window.innerWidth / 2, y: window.innerHeight / 4 };
+let lastElement;
+window.onscroll = (e) => {
+	const element = document
+		.elementFromPoint(center.x, center.y)
+		?.closest(".recipes__card");
+	if (!element) return lastElement?.classList.remove("recipes__card--expanded");
+
+	element.classList.add("recipes__card--expanded");
+	lastElement = element;
+};
